dfef8f2da0f7c6ff689b698b87f6f4d2fb3353e0
wsf <571017076@qq.com>
2019-07-12 11:14:55 +0800

    add recommend

---
+++ src/pages/home/components/HomeWeekend.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="(item, index) of weekendList" :key="index">
+        <div class="item-img-wrapper">
+           <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1812/b4/e9d7f043c4fadf.jpg_r_640x214_713df0cf.jpg',
+        title: '大连泡汤圣地',
+        desc: '享受生活，放松自己，来一段轻松的旅程。'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7f/fa4eb0ca72409d.jpg_r_640x214_2f2a860e.jpg',
+        title: '大连必打卡',
+        desc: '爱上北方的碧海蓝天'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/b7/8a08a1023062b3.jpg_r_640x214_d7cb475c.jpg',
+        title: '最赞的海滨浴场',
+        desc: '跳进海里，享受铺天盖地的清凉吧'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top :.2rem
+    line-height :.8rem
+    background :#eee
+    text-indent :.2rem
+  .item-img-wrapper
+    overflow :hidden
+    height :0
+    padding-bottom :33.9%
+  .item-img
+    width :100%
+  .item-info
+    padding :.1rem
+    .item-title
+      line-height :.54rem
+      font-size :.32rem
+      ellipsis()
+    .item-desc
+      line-height :.4rem
+      color :#ccc
+      ellipsis()
+</style>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="(item, index) of recommendList" :key="index">
+        <img class="item-img" :src="item.imgUrl">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_200x200_4ab5da75.jpg',
+        title: '大连圣亚海洋世界(4A)',
+        desc: '浪漫大连首站，浪漫的海洋主题公园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1808/d8/d89ec1b26e92573ea3.water.jpg_200x200_8606cb4d.jpg',
+        title: '海昌发现王国',
+        desc: '浪漫大连首站，奇妙的王国'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1903/89/8965f21723d9da86a3.water.jpg_200x200_90a1a827.jpg',
+        title: '大连森林动物园',
+        desc: '浪漫大连首站，可爱的动物主题公园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top :.2rem
+    line-height :.8rem
+    background :#eee
+    text-indent :.2rem
+  .item
+    overflow :hidden
+    display :flex
+    height :1.9rem
+    .item-img
+      width :1.7rem
+      height :1.7rem
+      padding :.1rem
+    .item-info
+      flex :1
+      padding :.1rem
+      min-width :0
+      .item-title
+        line-height :.54rem
+        font-size :.32rem
+        ellipsis()
+      .item-desc
+        line-height :.4rem
+        color :#ccc
+        ellipsis()
+      .item-button
+        line-height :.44rem
+        margin-top :.16rem
+        background :#ff9300
+        padding :0 .2rem
+        border-radius :.6rem
+        color :#fff
+</style>
